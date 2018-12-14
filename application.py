6af4fbd3441aef7dccb41a5ddc05c25181e6b2b2
Antoine Krajnc <antoinekrajnc@gmail.com>
2018-12-14 10:44:56 +0100

    implement dark_sky api

--- application.py
+++ application.py
@@ -1,29 +1,38 @@
-from flask import Flask, url_for, render_template, request, make_response
+from flask import Flask, url_for, render_template, request, make_response, json
+import requests
 app = Flask(__name__)
+
+dark_sky_api_key = "073ed950bcd367ad35e76ea60cf5511c"
+ipstack_api_key = "55896dde6c19b26566166b446fe84094"
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         return render_template("index.html", prenom="Michel")
     elif request.method == "POST":
-        nom = request.form["nom_utilisateur"]
-        email = request.form["email_utilisateur"]
-        message = request.form["message_utilisateur"]
         response = make_response(render_template("project.html"))
-        response.set_cookies("Nom","{}".format(nom))
         return response
 
 @app.route("/project", methods=["GET", "POST"])
 def project():
+    current_location = requests.get("http://api.ipstack.com/check", params={"access_key": ipstack_api_key})
+    current_location = current_location.json()
+    latitude = current_location["latitude"]
+    longitude = current_location["longitude"]
+    ville = current_location["city"]
+    weather = requests.get("https://api.darksky.net/forecast/{}/{},{}".format(dark_sky_api_key, latitude, longitude))
+    weather = weather.json()
     if request.method == "GET":
-        return render_template("project.html")
+        return render_template("project.html", weather = weather, ville = ville)
     elif request.method == "POST":
         nom = request.form["nom_utilisateur"]
         email = request.form["email_utilisateur"]
         message = request.form["message_utilisateur"]
-        return render_template("project.html", name = nom, email = email, message = message)
+        response = make_response(render_template("project.html",ville=ville,weather = weather ,name = nom, email = email, message = message))
+        response.set_cookie("Nom", nom)
+        return response
 
-@app.route("/urls")
+@app.route("/urls", methods=["GET", "POST"])
 def urls():
     nom = request.cookies.get("Nom")
     return render_template("url.html", nom = nom)
